d28a5a73c7e17c2e28892c3fa0b1bc860da1bfb6
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-28 14:28:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require "pry"
+class Dog
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    self.save
+    #binding.pry
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def save
+    @@all.push(self)
+  end
+  
+  def self.print_all
+    puts @@all.map {|dog|dog.name}
+  end
+  
+ #binding.pry
+end
